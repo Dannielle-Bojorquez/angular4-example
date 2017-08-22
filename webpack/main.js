abfab343ae60565919e3b9db3ebec4bb81f98ada
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-21 21:50:50 -0600

    bacon-service added

---
+++ webpack/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("../src/app/app.module");
+var core_1 = require("@angular/core");
+//only use when app is going live effects debugging
+core_1.enableProdMode();
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
